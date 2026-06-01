125b170528f5d7f1f74f957f3e18b5a343a25bd0
tstankevicius82-png <tstankevicius82@gmail.com>
2026-06-01 12:42:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def buy(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text("BUY WORKING")
+async def buy   (update: Update, context: ContextTypes.DEFAULT_TYPE):
+   await update.message.reply_text("BUY WORKING")
 
 app = Application.builder().token(TOKEN).build()
 
