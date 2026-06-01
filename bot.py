4cb1ac08c5047e12dc95eb0803f50e66057c60f3
tstankevicius82-png <tstankevicius82@gmail.com>
2026-06-01 12:17:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,9 @@
 import os
-from telegram.ext import Updater, CommandHandler
-
-TOKEN = os.getenv("BOT_TOKEN")
-
-def buy(update, context):
-update.message.reply_text("BUY WORKING")
-
-updater = Updater(TOKEN, use_context=True)
-
-dp = updater.dispatcher
-
-dp.add_handler(CommandHandler("buy", buy))
-
+from telegram.ext import Updater,CommandHandler
+TOKEN=os.getenv("BOT_TOKEN")
+def buy(update,context): update.message.reply_text("BUY WORKING")
+updater=Updater(TOKEN,use_context=True)
+dp=updater.dispatcher
+dp.add_handler(CommandHandler("buy",buy))
 updater.start_polling()
-
 updater.idle()
