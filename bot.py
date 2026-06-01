cc08422311d3164848cfb283a996ee927029976c
tstankevicius82-png <tstankevicius82@gmail.com>
2026-06-01 12:41:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
 import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN=os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-async def buy(update,context): await update.message.reply_text("BUY WORKING")
+async def buy(update: Update, context: ContextTypes.DEFAULT_TYPE):
+await update.message.reply_text("BUY WORKING")
 
-app=ApplicationBuilder().token(TOKEN).build()
+app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("buy",buy))
+app.add_handler(CommandHandler("buy", buy))
 
 app.run_polling()
