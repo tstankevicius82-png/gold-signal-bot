d91cc39ccd2544bffb48f861b1b89718cdf2d395
tstankevicius82-png <tstankevicius82@gmail.com>
2026-06-01 12:47:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,81 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def buy   (update: Update, context: ContextTypes.DEFAULT_TYPE):
-   await update.message.reply_text("BUY WORKING")
+# BUY COMMAND
+
+async def buy(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+```
+entry = float(context.args[0])
+
+tp1 = entry + 3
+tp2 = entry + 5
+tp3 = entry + 10
+sl = entry - 5
+
+msg = f"""
+```
+
+📊 GOLD SIGNAL — XAU/USD
+
+🟢 BUY
+
+━━━━━━━━━━━━
+📍 Entry: {entry}
+🛑 Stop Loss: {sl}
+
+🎯 TP1: {tp1}
+🎯 TP2: {tp2}
+🎯 TP3: {tp3}
+━━━━━━━━━━━━
+
+⚠️ Risk: 1%
+#gold #xauusd
+"""
+
+```
+await update.message.reply_text(msg)
+```
+
+# SELL COMMAND
+
+async def sell(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+```
+entry = float(context.args[0])
+
+tp1 = entry - 3
+tp2 = entry - 5
+tp3 = entry - 10
+sl = entry + 5
+
+msg = f"""
+```
+
+📊 GOLD SIGNAL — XAU/USD
+
+🔴 SELL
+
+━━━━━━━━━━━━
+📍 Entry: {entry}
+🛑 Stop Loss: {sl}
+
+🎯 TP1: {tp1}
+🎯 TP2: {tp2}
+🎯 TP3: {tp3}
+━━━━━━━━━━━━
+
+⚠️ Risk: 1%
+#gold #xauusd
+"""
+
+```
+await update.message.reply_text(msg)
+```
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("buy", buy))
+app.add_handler(CommandHandler("sell", sell))
 
 app.run_polling()
